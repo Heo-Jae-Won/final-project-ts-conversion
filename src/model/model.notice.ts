49774cd0ffcee05435adb36d022c7b9b9835c26a
Heo-Jae-Won <ckdwhgood2@naver.com>
2023-06-04 07:58:52 +0900

    notice 폴더 ts 변경 완료

---
+++ src/model/model.notice.ts
@@ -0,0 +1,8 @@
+export class Notice {}
+export interface Notice {
+  noticeCode: string;
+  noticeTitle: string;
+  noticeContent: string;
+  noticeWriter: string;
+  noticeRegDate: string;
+}
--- src/notice/NoticeItem.tsx
+++ src/notice/NoticeItem.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Notice } from "model/model.notice";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 /**
  * NoticeList의 실제 화면
  */
-const NoticeItem = ({ noticeList }) => {
-  const navigate=useNavigate();
-  const { noticeCode, noticeTitle,  noticeWriter, noticeRegDate } = noticeList;
+const NoticeItem = ({ noticeList }: { noticeList: Notice }) => {
+  const navigate = useNavigate();
+  const { noticeCode, noticeTitle, noticeWriter, noticeRegDate } = noticeList;
 
   return (
-    <tr style={{ cursor: 'pointer' }} onClick={() => navigate(`/notice/read/${noticeCode}`)}>
+    <tr
+      style={{ cursor: "pointer" }}
+      onClick={() => navigate(`/notice/read/${noticeCode}`)}
+    >
       <td>{noticeWriter}</td>
       <td>{noticeTitle}</td>
       <td>{noticeRegDate}</td>
     </tr>
-  )
-}
+  );
+};
 
 export default NoticeItem;
